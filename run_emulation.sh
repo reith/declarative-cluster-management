71849e2573d8aa98a2ee77a27707f04d8d696368
Lalith Suresh <lsuresh@vmware.com>
2020-09-01 12:51:07 -0700

    Checkpoint
    Signed-off-by: Lalith Suresh <lsuresh@vmware.com>

--- run_emulation.sh
+++ run_emulation.sh
@@ -7,10 +7,12 @@ GIT_BRANCH=`git rev-parse --abbrev-ref HEAD`
 
 mkdir -p $TRACE_DIR/$GIT_REV
 
-for numNodes in 500 5000 10000;
+startTimeCutOff=20000
+
+for affinityProportion in 0 50 100;
 do
-   startTimeCutOff=200000
-   ./gradlew runBenchmark --args="-n ${numNodes} -f v2-cropped.txt -c 100 -m 200 -t 100 -s ${startTimeCutOff} -p 100" &> /tmp/out
+   numNodes=500
+   ./gradlew runBenchmark --args="-n $numNodes -f v2-cropped.txt -c 100 -m 200 -t 100 -s ${startTimeCutOff} -p $affinityProportion" &> /tmp/out
    
    expId=`date +%s`
    mkdir -p $TRACE_DIR/$GIT_REV/$expId
@@ -18,5 +20,23 @@ do
    cp /tmp/out $TRACE_DIR/$GIT_REV/$expId/dcm_scheduler_trace
 
    echo "workload,schedulerName,solver,kubeconfig,dcmGitBranch,dcmGitCommitId,numNodes,startTimeCutOff,percentageOfNodesToScoreValue,timeScaleDown,affinityProportion" > $TRACE_DIR/$GIT_REV/$expId/metadata
-   echo "v2-cropped.txt,dcm-scheduler,ORTOOLS,local,$GIT_BRANCH,$GIT_REV,$numNodes,$startTimeCutOff,0,100,100" >> $TRACE_DIR/$GIT_REV/$expId/metadata
+   echo "v2-cropped.txt,dcm-scheduler,ORTOOLS,local,$GIT_BRANCH,$GIT_REV,$numNodes,$startTimeCutOff,0,100,$affinityProportion" >> $TRACE_DIR/$GIT_REV/$expId/metadata
+done
+
+
+TRACE_DIR=trace-`date +%s`
+mkdir -p $TRACE_DIR/$GIT_REV
+
+for numNodes in 500 5000 10000;
+do
+   affinityProportion=100
+   ./gradlew runBenchmark --args="-n ${numNodes} -f v2-cropped.txt -c 100 -m 200 -t 100 -s ${startTimeCutOff} -p ${affinityProportion}" &> /tmp/out
+   
+   expId=`date +%s`
+   mkdir -p $TRACE_DIR/$GIT_REV/$expId
+   cp /tmp/out $TRACE_DIR/$GIT_REV/$expId/workload_output
+   cp /tmp/out $TRACE_DIR/$GIT_REV/$expId/dcm_scheduler_trace
+
+   echo "workload,schedulerName,solver,kubeconfig,dcmGitBranch,dcmGitCommitId,numNodes,startTimeCutOff,percentageOfNodesToScoreValue,timeScaleDown,affinityProportion" > $TRACE_DIR/$GIT_REV/$expId/metadata
+   echo "v2-cropped.txt,dcm-scheduler,ORTOOLS,local,$GIT_BRANCH,$GIT_REV,$numNodes,$startTimeCutOff,0,100,$affinityProportion" >> $TRACE_DIR/$GIT_REV/$expId/metadata
 done
